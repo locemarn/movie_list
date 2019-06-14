4d979fb8a3bfdeb398d684a42361a016f863f23a
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-14 11:19:50 -0300

    config eslint and prettier

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,6 @@ import bodyParser from 'body-parser'
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 export default app
